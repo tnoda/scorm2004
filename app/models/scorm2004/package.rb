eabc63ca9d8a734327293b9b18cd1deb74738beb
Takahiro Noda <takahiro.noda@gmail.com>
2012-11-01 19:44:09 +0900

    Add the association test to the Package test.

--- app/models/scorm2004/package.rb
+++ app/models/scorm2004/package.rb
@@ -1,5 +1,6 @@
 module Scorm2004
   class Package < ActiveRecord::Base
+    has_many :content_organizations
     serialize :manifest, JSON
     attr_accessible :manifest
   end
--- test/unit/scorm2004/package_test.rb
+++ test/unit/scorm2004/package_test.rb
@@ -2,11 +2,8 @@ require 'test_helper' unless Spork.using_spork?
 
 module Scorm2004
   class PackageTest < ActiveSupport::TestCase
-    test 'the manifest column must be serialized as JSON.' do
-      assert Package.new do |pkg|
-        pkg.manifest = {:foo => :bar}
-        pkg.save
-      end
+    context 'a package' do
+      should have_many(:content_organizations)
     end
   end
 end
